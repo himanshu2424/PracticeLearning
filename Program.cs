57f2bbfd9d1541c6a98c54d4e605084c1fc97807
Lakhyani <himanshu.lakhyani@siemens.com>
2019-07-22 20:21:46 +0530

    added how are you

--- Program.cs
+++ Program.cs
@@ -11,6 +11,20 @@ namespace HelloDevops
         static void Main(string[] args)
         {
 
+		string[] arr = { "aaa.txt", "bbb.TXT", "xyz.abc.pdf", "aaaa.PDF", 
+                             "abc.xml", "ccc.txt", "zzz.txt" };
+          var egrp=arr.Select(file=>Path.GetExtension(file).TrimStart('.').ToLower())
+                     .GroupBy(x => x,(ext, extCnt) =>new
+                                                     {
+                                                        Extension = ext,
+                                                        Count = extCnt.Count()
+                                                      });
+ 
+          foreach (var v in egrp)
+              Console.WriteLine("{0} File(s) with {1} Extension ",
+                                  v.Count, v.Extension);
+								  
+			
             Console.WriteLine("Hello Devops by Himanshu for Jenkins !!. Automated builds are now successfull. Connected MSBUILD as well. Hi Mishti");
 			Console.ReadLine();
         }
